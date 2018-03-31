503f062115286c481f840c0089ca58b8060e5843
peatroot <garethpeat@gmail.com>
2018-03-31 22:33:53 +0100

    Add first pass at disease table query

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ let db = new sqlite3.Database('postgap.db', sqlite3.OPEN_READONLY, err => {
 
 // sqlite3 methods do not support promises out of the box
 db.all = promisify(db.all);
+db.get = promisify(db.get);
 
 // load the schema
 const schemaFile = path.join(__dirname, 'schema.gql');
@@ -135,6 +136,61 @@ const resolvers = {
                     diseases,
                     geneVariants,
                     variantLeadVariants
+                }
+            })
+        },
+        diseaseTable: (_, { efoId, offset, limit }) => {
+            const params = {$efoId: efoId, $offset: offset, $limit: limit};
+
+            // rows
+            const rowsSql = `
+            SELECT
+                gene_id as geneId,
+                gene_symbol as geneSymbol,
+                GRCH38_gene_chrom as geneChromosome,
+                GRCh38_gene_pos as geneTss,
+                ld_snp_rsID as variantId,
+                GRCH38_chrom as variantChromosome,
+                GRCh38_pos as variantPosition,
+                gwas_snp as leadVariantId,
+                disease_efo_id as efoId,
+                disease_name as efoName,
+                VEP as vep,
+                GTEx as gtex,
+                PCHiC as pchic,
+                Fantom5 as fantom5,
+                DHS as dhs,
+                Nearest as nearest,
+                r2,
+                gwas_pvalue as gwasPValue,
+                gwas_odds_ratio as gwasOddsRatio,
+                gwas_beta as gwasBeta,
+                gwas_size as gwasSize,
+                gwas_pmid as gwasPMId,
+                gwas_study as gwasStudy
+            FROM raw
+            WHERE disease_efo_id=$efoId
+            LIMIT $limit
+            OFFSET $offset
+            `;
+            const rowsQuery = db.all(rowsSql, params);
+
+            // total rows
+            const totalRowsSql = `
+            SELECT COUNT(*) as totalRows
+            FROM raw
+            WHERE disease_efo_id=$efoId
+            `;
+            const totalRowsQuery = db.get(totalRowsSql, { $efoId: efoId });
+
+            // wait for all queries and return composite object
+            return Promise.all([
+                rowsQuery,
+                totalRowsQuery,
+            ]).then(([rows, { totalRows }]) => {
+                return {
+                    rows,
+                    totalRows
                 }
             })
         }
