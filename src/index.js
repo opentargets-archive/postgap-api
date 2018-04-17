30a599184d6523c18de31e561ef1f007f9d11ff3
peatroot <garethpeat@gmail.com>
2018-04-17 11:09:34 +0100

    Support otG2VScore filter

--- src/index.js
+++ src/index.js
@@ -37,14 +37,17 @@ const typeDefs = fs.readFileSync(schemaFile, 'utf8');
 // specify the resolution methods for allowed query
 const resolvers = {
     Query: {
-        locus: (_, { chromosome, start, end, g2VMustHaves }) => {
+        locus: (_, { chromosome, start, end, g2VMustHaves, g2VScore }) => {
             const params = {$chromosome: chromosome, $start: start, $end: end};
             const oldWhere = `
             WHERE
                 (GRCh38_gene_chrom=$chromosome AND GRCh38_gene_pos>=$start AND GRCh38_gene_pos<=$end)
                 OR (GRCh38_chrom=$chromosome AND GRCh38_pos>=$start AND GRCh38_pos<=$end)
             `;
-            const filtersSql = g2VMustHaves.length > 0 ? (g2VMustHaves.map(d => `AND (${d.toLowerCase()} > 0)`).join(' ')) : '';
+            let filtersSql = g2VMustHaves.length > 0 ? (g2VMustHaves.map(d => `AND (${d.toLowerCase()} > 0)`).join(' ')) : '';
+            if (g2VScore && g2VScore.length === 2) {
+                filtersSql += ` AND (ot_g2v_score >= ${g2VScore[0]}) AND (ot_g2v_score <= ${g2VScore[1]})`;
+            }
             const templateWhere = filtersSql => `
             WHERE
                 (GRCh38_gene_start IS NOT NULL)
