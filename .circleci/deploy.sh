2f6a8cfae6178107534fb87c6f919ca247058154
Eliseo Papa <elipapa@users.noreply.github.com>
2018-04-26 01:11:35 +0100

    some deploy scripts (for now)

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,7 +1,4 @@
-gcloud compute ssl-certificates create wild-opentargets-io --certificate '*.opentargets.io.chained.crt' --private-key '*.opentargets.io.key' --project open-targets-eu-dev
+#!/usr/bin/env bash
+kubectl delete deploy/postgapapi
+kubectl run postgapapi --image=eu.gcr.io/open-targets-eu-dev/postgap-api:20180426sample --port=4000
 
-kubectl run postgapapi --image=eu.gcr.io/open-targets-eu-dev/postgap-api:24052018 --port=4000
-
-kubectl expose deployment postgapapi --target-port=4000 --type=NodePort
-
-kubectl create -f ingress.yml
