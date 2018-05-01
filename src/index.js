935bf8e7301a1234a06cf1d02f735ec3d12d0726
peatroot <garethpeat@gmail.com>
2018-05-01 11:25:40 +0100

    Measure several resolver execution times

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { buildSchema } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 import sqlite3 from 'sqlite3';
 import { promisify } from 'bluebird';
+import { addMiddleware } from 'graphql-add-middleware';
 
 // open a connection to the database
 let db = new sqlite3.Database('postgap.20180324.db', sqlite3.OPEN_READONLY, err => {
@@ -35,71 +36,85 @@ let leadVariantCache = {};
 const schemaFile = path.join(__dirname, 'schema.gql');
 const typeDefs = fs.readFileSync(schemaFile, 'utf8');
 
+// helper methods
+const getLocusFiltersSql = ({ g2VMustHaves, g2VScore, r2, gwasPValue }) => {
+    let filtersSql = g2VMustHaves.length > 0 ? (g2VMustHaves.map(d => `AND (${d.toLowerCase()} > 0)`).join(' ')) : '';
+    if (g2VScore && g2VScore.length === 2) {
+        filtersSql += ` AND (ot_g2v_score >= ${g2VScore[0]}) AND (ot_g2v_score <= ${g2VScore[1]})`;
+    }
+    if (r2 && r2.length === 2) {
+        filtersSql += ` AND (r2 >= ${r2[0]}) AND (r2 <= ${r2[1]})`;
+    }
+    if (gwasPValue && gwasPValue.length === 2) {
+        // note: need to do reverse -log10 conversion
+        filtersSql += ` AND (gwas_pvalue >= ${10 ** (-gwasPValue[1])}) AND (gwas_pvalue <= ${10 ** (-gwasPValue[0])})`;
+    }
+    return filtersSql
+}
+const templateWhere = filtersSql => `
+WHERE
+    (GRCh38_gene_start IS NOT NULL)
+    AND (GRCh38_gene_end IS NOT NULL)
+    AND (GRCh38_gwas_snp_pos IS NOT NULL)
+    ${filtersSql}
+    AND (
+        (GRCh38_gene_chrom=$chromosome AND GRCh38_gene_start>=$start AND GRCh38_gene_start<=$end)
+        OR (GRCh38_gene_chrom=$chromosome AND GRCh38_gene_end>=$start AND GRCh38_gene_end<=$end)
+        OR (GRCh38_chrom=$chromosome AND GRCh38_pos>=$start AND GRCh38_pos<=$end)
+        OR (GRCh38_gwas_snp_chrom=$chromosome AND GRCh38_gwas_snp_pos>=$start AND GRCh38_gwas_snp_pos<=$end))
+`;
+const getFilteredLocusWhereSql = (args) => {
+    const filtersSql = getLocusFiltersSql(args)
+    return templateWhere(filtersSql);
+}
+const getUnfilteredLocusWhereSql = () => {
+    return templateWhere('');
+}
+const getSelectedSql = ({ selectedId, selectedType }) => {
+    let selectedSql = '';
+    switch (selectedType) {
+        case 'gene':
+            selectedSql = `COUNT(CASE gene_id WHEN "${selectedId}" THEN 1 ELSE null END) > 0 AS selected,`;
+            break;
+        case 'variant':
+            selectedSql = `COUNT(CASE ld_snp_rsID WHEN "${selectedId}" THEN 1 ELSE null END) > 0 AS selected,`;
+            break;
+        case 'leadVariant':
+            selectedSql = `COUNT(CASE gwas_snp WHEN "${selectedId}" THEN 1 ELSE null END) > 0 AS selected,`;
+            break;
+        case 'disease':
+            selectedSql = `COUNT(CASE disease_efo_id WHEN "${selectedId}" THEN 1 ELSE null END) > 0 AS selected,`;
+            break;
+        case 'geneVariant':
+            const [geneId, variantId] = selectedId.split('-');
+            selectedSql = `COUNT(CASE ((gene_id = "${geneId}") AND (ld_snp_rsID = "${variantId}")) WHEN 1 THEN 1 ELSE null END) > 0 AS selected,`;
+            break;
+        case 'variantLeadVariant':
+            const [varId, leadVariantId] = selectedId.split('-');
+            selectedSql = `COUNT(CASE ((ld_snp_rsID = "${varId}") AND (gwas_snp = "${leadVariantId}")) WHEN 1 THEN 1 ELSE null END) > 0 AS selected,`;
+            break;
+        case 'leadVariantDisease':
+            const [leadVarId, diseaseId] = selectedId.split('-');
+            selectedSql = `COUNT(CASE ((gwas_snp = "${leadVarId}") AND (disease_efo_id = "${diseaseId}")) WHEN 1 THEN 1 ELSE null END) > 0 AS selected,`;
+            break;
+        default:
+            break;
+    }
+    return selectedSql;
+}
+
 // specify the resolution methods for allowed query
 const resolvers = {
     Query: {
-        locus: (_, { chromosome, start, end, g2VMustHaves, g2VScore, r2, gwasPValue, selectedId, selectedType }) => {
+        locus: (_, args) => {
+            const { chromosome, start, end, g2VMustHaves, g2VScore, r2, gwasPValue, selectedId, selectedType } = args;
             const params = {$chromosome: chromosome, $start: start, $end: end};
-            const oldWhere = `
-            WHERE
-                (GRCh38_gene_chrom=$chromosome AND GRCh38_gene_pos>=$start AND GRCh38_gene_pos<=$end)
-                OR (GRCh38_chrom=$chromosome AND GRCh38_pos>=$start AND GRCh38_pos<=$end)
-            `;
-            let filtersSql = g2VMustHaves.length > 0 ? (g2VMustHaves.map(d => `AND (${d.toLowerCase()} > 0)`).join(' ')) : '';
-            if (g2VScore && g2VScore.length === 2) {
-                filtersSql += ` AND (ot_g2v_score >= ${g2VScore[0]}) AND (ot_g2v_score <= ${g2VScore[1]})`;
-            }
-            if (r2 && r2.length === 2) {
-                filtersSql += ` AND (r2 >= ${r2[0]}) AND (r2 <= ${r2[1]})`;
-            }
-            if (gwasPValue && gwasPValue.length === 2) {
-                // note: need to do reverse -log10 conversion
-                filtersSql += ` AND (gwas_pvalue >= ${10 ** (-gwasPValue[1])}) AND (gwas_pvalue <= ${10 ** (-gwasPValue[0])})`;
-            }
-            const templateWhere = filtersSql => `
-            WHERE
-                (GRCh38_gene_start IS NOT NULL)
-                AND (GRCh38_gene_end IS NOT NULL)
-                AND (GRCh38_gwas_snp_pos IS NOT NULL)
-                ${filtersSql}
-                AND (
-                    (GRCh38_gene_chrom=$chromosome AND GRCh38_gene_start>=$start AND GRCh38_gene_start<=$end)
-                    OR (GRCh38_gene_chrom=$chromosome AND GRCh38_gene_end>=$start AND GRCh38_gene_end<=$end)
-                    OR (GRCh38_chrom=$chromosome AND GRCh38_pos>=$start AND GRCh38_pos<=$end)
-                    OR (GRCh38_gwas_snp_chrom=$chromosome AND GRCh38_gwas_snp_pos>=$start AND GRCh38_gwas_snp_pos<=$end))
-            `;
-            const filteredWhere = templateWhere(filtersSql);
-            const unfilteredWhere = templateWhere('');
             
-            let selectedSql = '';
-            switch (selectedType) {
-                case 'gene':
-                    selectedSql = `COUNT(CASE gene_id WHEN "${selectedId}" THEN 1 ELSE null END) > 0 AS selected,`;
-                    break;
-                case 'variant':
-                    selectedSql = `COUNT(CASE ld_snp_rsID WHEN "${selectedId}" THEN 1 ELSE null END) > 0 AS selected,`;
-                    break;
-                case 'leadVariant':
-                    selectedSql = `COUNT(CASE gwas_snp WHEN "${selectedId}" THEN 1 ELSE null END) > 0 AS selected,`;
-                    break;
-                case 'disease':
-                    selectedSql = `COUNT(CASE disease_efo_id WHEN "${selectedId}" THEN 1 ELSE null END) > 0 AS selected,`;
-                    break;
-                case 'geneVariant':
-                    const [geneId, variantId] = selectedId.split('-');
-                    selectedSql = `COUNT(CASE ((gene_id = "${geneId}") AND (ld_snp_rsID = "${variantId}")) WHEN 1 THEN 1 ELSE null END) > 0 AS selected,`;
-                    break;
-                case 'variantLeadVariant':
-                    const [varId, leadVariantId] = selectedId.split('-');
-                    selectedSql = `COUNT(CASE ((ld_snp_rsID = "${varId}") AND (gwas_snp = "${leadVariantId}")) WHEN 1 THEN 1 ELSE null END) > 0 AS selected,`;
-                    break;
-                case 'leadVariantDisease':
-                    const [leadVarId, diseaseId] = selectedId.split('-');
-                    selectedSql = `COUNT(CASE ((gwas_snp = "${leadVarId}") AND (disease_efo_id = "${diseaseId}")) WHEN 1 THEN 1 ELSE null END) > 0 AS selected,`;
-                    break;
-                default:
-                    break;
-            }
+            
+            const filteredWhere = getFilteredLocusWhereSql(args);
+            const unfilteredWhere = getUnfilteredLocusWhereSql();
+            
+            let selectedSql = getSelectedSql(args);
             const orderBySql = selectedSql ? 'ORDER BY selected' : '';
 
             // genes
@@ -493,6 +508,23 @@ const resolvers = {
 // merge schema with resolvers
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
+// time specific resolver calculations
+addMiddleware(schema, 'Query.locus', async (rootValue, args, context, info, next) => {
+    const start = Date.now();
+    const result = await next();
+    const end = Date.now();
+    console.log(`Query.locus took: ${end - start}ms`);
+    return result;
+})
+addMiddleware(schema, 'Query.locus.genes', async (rootValue, args, context, info, next) => {
+    const start = Date.now();
+    const result = await next();
+    const end = Date.now();
+    console.log(`Query.locus took: ${end - start}ms`);
+    return result;
+})
+
+// setup cors options
 const corsOptions = {
     preflightContinue: false
 };
