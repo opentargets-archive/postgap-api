5f7cd27ba44a17675fb97cce29f2d5510662b442
peatroot <garethpeat@gmail.com>
2018-05-03 13:55:44 +0100

    Load the db into memory on init

--- src/index.js
+++ src/index.js
@@ -14,18 +14,47 @@ import locus, { resolveLocus } from './resolvers/locus';
 import locusTable from './resolvers/locusTable';
 import diseaseTable from './resolvers/diseaseTable';
 
-// open a connection to the database
-let db = new sqlite3.Database('postgap.20180324.db', sqlite3.OPEN_READONLY, err => {
+const DB_FILENAME = 'postgap.20180324.db';
+
+// open a connection to an in-memory database
+let db = new sqlite3.Database(':memory:', sqlite3.OPEN_READWRITE, err => {
     if (err) {
         console.error(err.message)
     } else {
-        console.log('Connected to POSTGAP database.')
-    }
-})
+        // sqlite3 methods do not support promises out of the box
+        db.all = promisify(db.all);
+        db.get = promisify(db.get);
+        db.exec = promisify(db.exec);
+        db.run = promisify(db.run);
 
-// sqlite3 methods do not support promises out of the box
-db.all = promisify(db.all);
-db.get = promisify(db.get);
+        // temporarily attach the file db and copy into memory
+        db.run(`ATTACH DATABASE "${DB_FILENAME}" AS filedb`)
+        .then(() => console.log('Attached file db.'))
+        .then(() => {
+            return db.all('SELECT type,name,sql FROM filedb.sqlite_master;').then(data => {
+                const buildSql = data.map(d => d.sql).join(';');
+                const tables = data.filter(d => d.type === 'table')
+                    .map(d => d.name);
+
+                const copyTables = () => Promise.all(
+                    tables.map(table => {
+                        return db.exec(`INSERT INTO ${table} SELECT * FROM filedb.${table};`)
+                            .then(() => console.log(`Copied all rows of ${table} into memory.`))
+                    })
+                );
+                return db.exec(buildSql).then(() => console.log('Created tables in memory.'))
+                .then(copyTables);
+            })
+        })
+        .then(() => {
+            return db.run(`DETACH DATABASE filedb`);
+        })
+        .then(() => console.log('Detached file db.'))
+        .then(() => {
+            initGeneLocationCache(db);
+        });
+    }
+});
 
 // sqlite3 emits a profile event whenever sql is executed
 // use this to log query execution times (where the sql is commented
@@ -42,26 +71,28 @@ db.on('profile', (sql, ms) => {
 // total number of genes is ~20,000,
 // so cache ensembl calls on an object
 let geneLocationsCache = {};
-const geneLocationsSql = `
--- SQL_QUERY_TYPE=loadGeneLocations
-SELECT DISTINCT
-    gene_id as geneId,
-    description,
-    strand,
-    canonical_transcript as canonicalTranscript
-FROM gene
-`;
-db.all(geneLocationsSql).then(genes => {
-    // create a lookup cache
-    genes.forEach(d => {
-        geneLocationsCache[d.geneId] = {
-            geneId: d.geneId,
-            description: d.description,
-            forwardStrand: (d.strand === 1),
-            canonicalTranscript: JSON.parse(d.canonicalTranscript)
-        }
+function initGeneLocationCache(db) {
+    const geneLocationsSql = `
+    -- SQL_QUERY_TYPE=loadGeneLocations
+    SELECT DISTINCT
+        gene_id as geneId,
+        description,
+        strand,
+        canonical_transcript as canonicalTranscript
+    FROM gene
+    `;
+    db.all(geneLocationsSql).then(genes => {
+        // create a lookup cache
+        genes.forEach(d => {
+            geneLocationsCache[d.geneId] = {
+                geneId: d.geneId,
+                description: d.description,
+                forwardStrand: (d.strand === 1),
+                canonicalTranscript: JSON.parse(d.canonicalTranscript)
+            }
+        });
     });
-});
+}
 
 // load the schema
 const schemaFile = path.join(__dirname, 'schema.gql');
