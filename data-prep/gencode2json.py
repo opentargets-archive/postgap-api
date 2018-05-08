cb10a421425d64373feb47312ff5d4fac960ab82
Eliseo Papa <elipapa@users.noreply.github.com>
2018-05-08 12:03:32 +0100

    output json from mysql ensembl db calls

---
+++ data-prep/gencode2json.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import pandas as pd
+
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql+mysqldb://anonymous@ensembldb.ensembl.org/homo_sapiens_core_92_38')
+
+
+q = """
+select
+et.exon_id,
+et.transcript_id,
+g.stable_id as id,
+g.description,
+r.name as chr,
+g.seq_region_start as start,
+g.seq_region_end as end,
+e.seq_region_start as exon_start,
+e.seq_region_end as exon_end,
+t.seq_region_strand as fwdstrand
+from exon_transcript et, exon e, gene g, transcript t, seq_region r
+where
+g.canonical_transcript_id = et.transcript_id and
+g.seq_region_id = r.seq_region_id and
+r.coord_system_id = 4 and
+et.transcript_id = t.transcript_id and
+e.exon_id =et.exon_id
+"""
+
+df = pd.read_sql_query(q, engine,index_col='exon_id')
+df['exons'] = list(zip(df.exon_start, df.exon_end))
+df['fwdstrand'] = df['fwdstrand'].map({1:True,-1:False})
+df['tss'] = df.apply(lambda row: row['start'] if row['fwdstrand'] else row['end'], axis=1)
+flatdf = pd.DataFrame(df.groupby(['id','description','tss','chr','start','end','fwdstrand'])['exons'].apply(list)).reset_index()
+flatdf.set_index('id', inplace=True)
+print(flatdf['chr'].value_counts())
+flatdf.to_json('genes.json',orient='index')
+# print(df.groupby(['stable_id','description','strand'])['exon'].apply(list(zip(df.exon))))
+# out = c.fetchall()
+
